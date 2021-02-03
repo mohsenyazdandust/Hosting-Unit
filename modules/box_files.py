6256623cdfbd825bb820e87c1dc01d8c3db28ea4
themohsen <m.yazdandust1403@gmail.com>
2021-02-03 21:55:10 +0330

    Fixed Bugs

--- modules/box_files.py
+++ modules/box_files.py
@@ -1,8 +1,25 @@
 def get_files():
-	from os import listdir
-	from os.path import isfile, join
+	import os 
+	import time
 	import pathlib
 
-	directory = join(pathlib.Path().absolute(), "box")
-	files = [file for file in listdir(directory) if isfile(join(directory, file))]
+	files = []
+	directory = os.path.join(pathlib.Path().absolute(), "box")
+
+	for file in os.listdir(directory):
+		file_dir = os.path.join(directory, file)
+		file_size = os.path.getsize(file_dir)
+		size_m = "B"
+		if file_size >= 1024 * 1024:
+			file_size /= 1024 * 1024
+			size_m = "GB"
+		elif file_size >= 1024:
+			file_size /= 1024
+			size_m = "MB"
+		file_dict = {'Name': file,
+			"Date": time.strftime('%Y/%d/%m',time.gmtime(os.path.getmtime(file_dir))),
+			"Size": file_size,
+			"Size_m": size_m}
+		files.append(file_dict)
+
 	return files
